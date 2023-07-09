d7922eec75041af8dacb474f350cb39e19ddd22b
FaresCherni <fares.cherni.31@gmail.com>
2023-07-09 09:35:17 +0100

    video Animation

--- Iffirmations/View/Home/Themes/VideoCard.swift
+++ Iffirmations/View/Home/Themes/VideoCard.swift
@@ -59,7 +59,7 @@ struct VideoCard: View {
 
 struct VideoCardPager: View {
     @StateObject var themeVM : ThemeViewModel = ThemeViewModel.shared
-
+    @State var refresher : Bool = false
     let width : CGFloat = (UIScreen.main.bounds.width - 16 * 5) / 3
     var video :  AnnimatedThemesModel? {
       AnnimatedThemesModel.animatedThemes.first(where: { video in video.id == themeVM.AnimatedVidID})
@@ -82,9 +82,13 @@ struct VideoCardPager: View {
         ZStack(alignment: .topTrailing){
             ZStack {
                 if let urlToPlay = urlToPlay {
-                    PlayerViewController(url: urlToPlay)
-                        .frame(width: UIScreen.main.bounds.width)
-                        .frame(height: UIScreen.main.bounds.height - 44 - 64 - 48 - top - bottom)
+                    if refresher {
+                        videoToPlay(url: urlToPlay)
+                    }
+                    else {
+                        videoToPlay(url: urlToPlay)
+                    }
+
                 }
                 else if let video = video {
                     Image(video.id)
@@ -99,41 +103,129 @@ struct VideoCardPager: View {
        
             
         }
-    }
-    
-}
-
-
-struct PlayerViewController: UIViewControllerRepresentable {
-  
-    let url : URL
-    func makeUIViewController(context: Context) -> AVPlayerViewController {
-        let player: AVPlayer = AVPlayer(url: url)
-        let controller = AVPlayerViewController()
-        
-        controller.modalPresentationStyle = .automatic
-        controller.player = player
-    
-        // Loop the video
-        controller.player?.actionAtItemEnd = .none
-        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: nil, queue: .main) { _ in
-            controller.player?.seek(to: CMTime.zero)
-            controller.player?.play()
+        .onChange(of: themeVM.AnimatedVidID) { newValue in
+            refresher.toggle()
         }
-        controller.showsPlaybackControls = false
-        controller.videoGravity = .resizeAspectFill
-        controller.player?.play()
-        return controller
+    }
+    
+    func  videoToPlay(url : URL ) -> some View {
+        PlayerViewController(url: url)
+            .frame(width: UIScreen.main.bounds.width)
+            .frame(height: UIScreen.main.bounds.height - 44 - 64 - 48 - top - bottom)
+    }
+    
+}
+
+//
+//struct PlayerViewController: UIViewControllerRepresentable {
+//
+//    let url : URL
+//    func makeUIViewController(context: Context) -> AVPlayerViewController {
+//        let player: AVPlayer = AVPlayer(url: url)
+//        let controller = AVPlayerViewController()
+//
+//        controller.modalPresentationStyle = .automatic
+//        controller.player = player
+//
+//        // Loop the video
+//        controller.player?.actionAtItemEnd = .none
+//        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: nil, queue: .main) { _ in
+//            controller.player?.seek(to: CMTime.zero)
+//            controller.player?.play()
+//        }
+//        controller.showsPlaybackControls = false
+//        controller.videoGravity = .resizeAspectFill
+//        controller.player?.play()
+//        return controller
+//    }
+//
+//    func updateUIViewController(_ playerController: AVPlayerViewController, context: Context) {
+//        playerController.player?.pause()
+//        playerController.player?.seek(to: .zero)
+//        playerController.player = AVPlayer(url: url)
+//        playerController.player?.play()
+//    }
+//}
+//
+//
+//
+//
+
+//struct PlayerViewController: UIViewControllerRepresentable {
+//
+//    let url : URL
+//
+//    func makeUIViewController(context: Context) -> AVPlayerViewController {
+//        let player: AVQueuePlayer = AVQueuePlayer()
+//        let controller = AVPlayerViewController()
+//
+//        controller.modalPresentationStyle = .automatic
+//        controller.player = player
+//
+//        // Loop the video
+//        let playerItem = AVPlayerItem(url: url)
+//        let playerLooper = AVPlayerLooper(player: player, templateItem: playerItem)
+//
+//        controller.showsPlaybackControls = false
+//        controller.videoGravity = .resizeAspectFill
+//        controller.player?.play()
+//
+//        return controller
+//    }
+//
+//    func updateUIViewController(_ playerController: AVPlayerViewController, context: Context) {
+////        playerController.player?.pause()
+////        playerController.player?.seek(to: .zero)
+////        playerController.player?.replaceCurrentItem(with: AVPlayerItem(url: url))
+////        playerController.player?.play()
+//    }
+//}
+
+
+
+
+struct PlayerViewController: UIViewRepresentable {
+    let url : URL
+    
+    func updateUIView(_ uiView: LoopingPlayerUIView, context: UIViewRepresentableContext<PlayerViewController>) {
+
     }
 
-    func updateUIViewController(_ playerController: AVPlayerViewController, context: Context) {
-        playerController.player?.pause()
-        playerController.player?.seek(to: .zero)
-        playerController.player = AVPlayer(url: url)
-        playerController.player?.play()
+    func makeUIView(context: Context) -> LoopingPlayerUIView {
+        return LoopingPlayerUIView(frame: .zero,url : url)
     }
 }
 
 
+class LoopingPlayerUIView: UIView {
+     let playerLayer = AVPlayerLayer()
+     var playerLooper: AVPlayerLooper?
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+   init(frame: CGRect, url : URL) {
+        super.init(frame: frame)
+
+        let item = AVPlayerItem(url: url)
+        
+        // Setup the player
+        let player = AVQueuePlayer()
+        player.volume = 0.0
+         
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+         
+        // Create a new player looper with the queue player and template item
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+        // Start the movie
+        player.play()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+}
